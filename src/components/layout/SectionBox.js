0fb064e017d37f23f0671732a0f29bd51f752501
Shawn Kebel <fxrender@outlook.com>
2024-01-17 20:45:58 -0600

    :building_construction::lipstick: Add SectionBox component with updated background color

---
+++ src/components/layout/SectionBox.js
@@ -0,0 +1,6 @@
+export default function SectionBox({ children }) {
+  return (
+    // <div className="bg-white md:m-8 md:p-4 m-5 p-4 shadow">{children}</div>
+    <div className="bg-[#121212] md:m-8 md:p-4 m-5 p-4 shadow">{children}</div>
+  );
+}
